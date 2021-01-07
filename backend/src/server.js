968ab5ee7e136a4d44275d0cdd85b238f5585b1a
jardielson <jardielsonbio@gmail.com>
2021-01-07 20:14:19 -0300

    maked routes of list and register

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,58 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
-app.use(cors());
 app.use(express.json());
 
+app.use(cors());
+
+
+
+const metas = require('./model/db');
+const { response } = require('express');
+
+function sleep(ms){
+  return new Promise((resolve)=>{
+      setTimeout(resolve,ms)
+  })
+}
+
+app.post("/metas",async (req,res) => {
+
+    await sleep(300);
+
+    const {name, decription, status} = req.body;
+
+ try{
+    await metas.create({
+        name,
+        decription,
+        status
+    }).then((metas) => {
+
+        return res.status(200).json({message:"goals registed with sucess"});
+    })
+    .catch((err)=>{
+        return res.status(501).json(err);
+    });
+ }catch(err){
+    return res.status(500).json("error to connect with databse");
+}
+});
+
+app.get("/listar",async (req,res)=>{
+     
+try{
+    const datas = await metas.find()
+    .then((datas)=>{
+        return res.status(200).json( { datas } );
+    })
+    .catch((err)=>{
+        return res.status(404).json("error when finding file");
+    })
+}catch(err){
+    return res.status(500).json("error to connect with databse");
+}
+});
 
 app.listen(8081);
-
-module.exports = app;
--- frontend/pages/register.js
+++ frontend/pages/register.js
@@ -3,31 +3,51 @@ import React, { useState } from 'react';
 function Register(){
 
     const [meta , setMeta] = useState({
-        name: "",
-        description: "",
-        status: ""
+        name: '',
+        decription: '',
+        status: ''
+    });
+
+    const [ response , setResponse] = useState({
+        formSave: false,
+        type: '',
+        message:''
     });
 
     const onChangeInput = e => setMeta({...meta, [ e.target.name]: e.target.value});
     const sendMeta = async e => {
         e.preventDefault();
-        
-        
-        console.log(meta);
+
 
      try{
         const res = await fetch('http://localhost:8081/metas',{
             method: 'POST',
             body: JSON.stringify(meta),
+            headers: { 'Content-type': 'application/json' }
         })
         const message = await res.json();
-        if(message.err){
-            console.log(message.err)
+
+        
+        if(message.response){
+            setResponse({
+                formSave: false,
+                type: 'error',
+                message:message.response
+            })
         }else{
-            console.log("register with sucessful")
+            setResponse({
+                formSave: false,
+                type: 'sucess',
+                message:message.response
+            })
+            //console.log(response)
         }
     }catch(err){
-        console.log("error: goal not register without sucessful")
+        setResponse({
+            formSave: false,
+            type: 'error: goal not register without sucessful',
+            message:message.err
+        })
     }
     }
 
@@ -35,17 +55,23 @@ function Register(){
            <div onSubmit={sendMeta}>
              <h1>register my goals </h1>
              <hr/>
+             {
+                 response.type === 'error' ? <p>{ response.message}</p> : ""
+             }
+             {
+                 response.type === 'sucess' ? <p>{ response.message}</p> : ""
+             }
              <form>
                  <label> name </label>
-                 <input tyep="text" name="name" id="name" placeholder="goal" onChange={onChangeInput}/>
+                 <input tyep="text" name="name" id="name" placeholder="goal" onChange={onChangeInput} required/>
                  <br/>
                  <br/>
                  <label> description </label>
-                 <input tyep="text" name="decription" id="decription" placeholder="description" onChange={onChangeInput}/>
+                 <input tyep="text" name="decription" id="decription" placeholder="description" onChange={onChangeInput} required/>
                  <br/>
                  <br/>
                  <label> status </label>
-                 <input tyep="text" name="status" id="status" placeholder="status" onChange={onChangeInput}/>
+                 <input tyep="text" name="status" id="status" placeholder="status" onChange={onChangeInput} required/>
                  <br/>
                  <br/>
                  <button type="submit"> register </button>
